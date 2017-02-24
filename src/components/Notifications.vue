1d988e983ad67e13dfa4db11277887ad48e7ff13
David Martinez <davidmgilo@gmail.com>
2017-02-24 17:25:19 +0100

    request permission notifications

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     {{ msg }}
+    <input type="button" value="show noti" @click="showNotification">
   </div>
 </template>
 
@@ -11,6 +12,19 @@ export default {
     return {
       msg: 'Todo!!'
     }
+  },
+  methods: {
+    showNotification: function () {
+      console.log('TODO')
+    }
+  },
+  created () {
+    if (!('Notification' in window)) {
+      alert('This browser does not support system notifications')
+    }
+    Notification.requestPermission().then(function (result) {
+      console.log(result)
+    })
   }
 }
 </script>
